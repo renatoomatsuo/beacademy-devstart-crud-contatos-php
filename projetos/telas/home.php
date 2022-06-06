db5bc28767258b4a1324dc1f42d4a17ee59e2b5e
Renato Matsuo Bezerra Tanaka <101281804+renatoomatsuo@users.noreply.github.com>
2022-06-06 19:05:11 -0300

    Create home.php

---
+++ projetos/telas/home.php
@@ -0,0 +1,10 @@
+<h1>Pagina Inicial</h1>
+<hr>
+
+<div>
+    <p>
+    Projeto de execução das aulas de PHP do Programa DevStart da BeAcademy em parceria com a Paylivre. 
+    </p>
+</div>
+
+<img src="telas/img/welcome.svg"  width="40%">
